96ffb2bf774a77b4e38f2ecc27f20a5f5a5ae415
BaranKaratass <157041449+BaranKaratass@users.noreply.github.com>
2026-03-29 12:47:04 +0300

    feat: implement AppRouter with Auth routes

---
+++ src/AppRouter.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext';
+
+const AppRouter = () => {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route 
+            path="/" 
+            element={
+              <ProtectedRoute>
+                <App />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
+export default AppRouter;
